5a37227adfc722a7106def3b13d9f293f64f64b2
Anmol Arora <anmolarora885@gmail.com>
2023-07-15 01:16:47 +0530

    added websocket to display logs

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from enum import Enum
+from fastapi.websockets import WebSocket,WebSocketDisconnect
+import time
+import random
 
 templates = Jinja2Templates(directory="template")
 app = FastAPI()
@@ -34,3 +37,46 @@ async def receive_form_data(form_data: dict):
     # Process the received form data
     print(form_data)
     return {"message": "Form data received"}
+
+
+@app.get("/progress")
+async def progress(request: Request):
+    return templates.TemplateResponse("progress.html" ,{"request": request})
+
+
+connected_clients = set()
+
+@app.websocket("/ws/logs")
+async def websocket_logs(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.add(websocket)
+
+    try:
+        while True:
+            # Continuously send log messages to the client
+            log_message =  get_next_log_message()
+            await websocket.send_text(log_message)
+    except WebSocketDisconnect:
+        connected_clients.remove(websocket)
+
+
+
+log_messages = [
+    "This is an informational log message",
+    "This is another log message",
+    "An error occurred",
+    "Warning: Invalid input detected",
+    "Debugging information"
+]
+
+def get_next_log_message():
+    # Simulate fetching the next log message from a logging system
+    # Replace this logic with your actual implementation
+
+    # Randomly select a log message from the list
+    log_message = random.choice(log_messages)
+    
+    # Simulate some processing time
+    time.sleep(1)
+
+    return log_message
